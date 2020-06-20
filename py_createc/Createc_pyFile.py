8e7c6df718ea62f5d846ca6d125b4ddd9112e4f0
Chen <chen.xu@aalto.fi>
2020-06-20 13:47:21 +0300

    correct a typo
    img_pixel should be tuple (y, x) instead of list [y, x]

--- py_createc/Createc_pyFile.py
+++ py_createc/Createc_pyFile.py
@@ -171,7 +171,7 @@ class DAT_IMG:
         # imgs are numpy arrays, with rows with only zeros cropped off
         self.imgs = [self._crop_img(arr) for arr in self.img_array_list]
         assert(len(set(img.shape for img in self.imgs)) <= 1)
-        self.img_pixels = self.imgs[0].shape # size in [y, x]
+        self.img_pixels = self.imgs[0].shape # size in (y, x)
         
     def _extracted_meta(self):
         """
