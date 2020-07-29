8ef0db9edb87ada896a293482151bc5cfdd0b640
Chen <chen.xu@aalto.fi>
2020-07-29 09:52:39 +0300

    Add method for rotating point when y axis is inverted in misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -12,3 +12,10 @@ def point_rot2D(target=XY2D(1,1), origin=XY2D(0,0), radians=0):
     	               y = target.y - origin.y)
     return XY2D(x = origin.x + cos_rad * adjusted.x - sin_rad * adjusted.y,
     	           y = origin.y + sin_rad * adjusted.x + cos_rad * adjusted.y)
+
+def point_rot2D_y_inv(target=XY2D(1,1), origin=XY2D(0,0), radians=0):
+
+    result = point_rot2D(target=XY2D(x=target.x, y=-target.y), 
+    	                 origin=XY2D(x=origin.x, y=-origin.y),
+    	                 radians=radians)
+    return XY2D(x=result.x, y=-result.y)
