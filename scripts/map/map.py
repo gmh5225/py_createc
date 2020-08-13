5d3334209b642e268bffbe62bb9d91e65554876e
Chen <chen.xu@aalto.fi>
2020-08-13 20:58:22 +0300

    Add more capabilities to the map script
    1. User can select which channel to show after uploading a file.
    2. User can re-connect to the STM software if connection is lost..

--- scripts/map/map.py
+++ scripts/map/map.py
@@ -6,11 +6,11 @@ from bokeh.server.server import Server
 from bokeh.models.tools import PanTool, BoxZoomTool, WheelZoomTool, \
 UndoTool, RedoTool, ResetTool, SaveTool, HoverTool
 from bokeh.palettes import Greys256
-from bokeh.models import Button, HoverTool, TapTool, TextInput, CustomJS
+from bokeh.models import Button, HoverTool, TapTool, TextInput, CustomJS, Select
 from bokeh.events import Tap, DoubleTap
 
 import base64
-from collections import deque
+from collections import deque, namedtuple
 import matplotlib.pyplot as plt
 import tornado.web
 import numpy as np
@@ -28,6 +28,8 @@ IMAGE_CHANNEL1 = 1 # channel current
 SCAN_BOUNDARY_X = 6000 # scanner range in angstrom
 SCAN_BOUNDARY_Y = 6000
 NUM_SIGMA = 3 # remove any outlier pixels of an image beyond a defined several sigmas
+MAX_CH = 8
+FILE_TUPLE = namedtuple('FILE_TUPLE', ['file', 'filename'])
 
 def make_document(doc):
 
@@ -49,6 +51,9 @@ def make_document(doc):
         """
         Callback for Double tap to mark a new scan area in the map
         """
+        if stm is None:
+            print('No STM is connected')
+            return
         assert ',' in textxy_tap.value, 'A valid coordinate string should contain a comma'
         x, y = textxy_tap.value.split(',')
         x = float(x)
@@ -84,67 +89,100 @@ def make_document(doc):
         print('XY coordinate sent')
         print(f'x={x} y={y}')
 
-    def upload_data(attr, old, new):
+    def plot_img():
         """
-        Callback to upload image to the map
+        The main function to plot image onto the frame
+        """
+
+        if file_holder is None:
+            return
+        file = file_holder.file
+        filename = file_holder.filename
+
+        channel = int(ch_select.value[-1])
+        if channel >= file.channels:
+            channel = file.channels-1
+            ch_select.value = f'{ch_select.value[:-1]}{channel}'
+
+        img = file.imgs[channel]
+            # img = level_correction(file.imgs[IMAGE_CHANNEL0])
+
+        # remove any outlier
+        threshold = np.mean(img)+NUM_SIGMA*np.std(img)
+        img[img>threshold] = threshold
+        threshold = np.mean(img)-NUM_SIGMA*np.std(img)
+        img[img<threshold] = threshold
+
+        temp = file.nom_size.y-file.size.y if file.scan_ymode == 2 else 0
+        anchor = XY2D(x=file.offset.x, 
+                      y=(file.offset.y+temp+file.size.y/2))
+
+        anchor = point_rot2D_y_inv(anchor, XY2D(x=file.offset.x, y=file.offset.y), 
+                             np.deg2rad(file.rotation))
+        # print('offset:', file.offset)
+        # print('angle:', file.rotation)
+        if int(file.rotation*100) not in [0, 9000, -9000, 18000, -18000]:
+            temp_file_name = f'image{filename}_{channel}.png'
+            path = os.path.join(os.path.dirname(__file__), 'temp', temp_file_name)
+            
+            plt.imsave(path, img, cmap='gray')
+            p.image_url([temp_file_name], x=anchor.x, y=anchor.y, anchor='center',
+                                     w=file.size.x, h=file.size.y, 
+                                     angle=file.rotation, 
+                                     angle_units='deg',
+                                     name = filename)
+            path_que.append(path)
+            return None                
+        elif int(file.rotation*100) == 0:
+            anchor = XY2D(x=anchor.x-file.size.x/2, y=anchor.y+file.size.y/2)
+            width = file.size.x
+            height = file.size.y
+        elif int(file.rotation*100) == 9000:
+            img = img.swapaxes(-2,-1)[...,::-1,:]
+            anchor = XY2D(x=anchor.x-file.size.y/2, y=anchor.y+file.size.x/2)
+            width = file.size.y
+            height = file.size.x
+        elif int(file.rotation*100) == -9000:
+            img = img.swapaxes(-2,-1)[...,::-1]
+            anchor = XY2D(x=anchor.x-file.size.y/2, y=anchor.y+file.size.x/2)
+            width = file.size.y
+            height = file.size.x
+        else:
+            img = img[...,::-1,::-1]
+            anchor = XY2D(x=anchor.x-file.size.x/2, y=anchor.y+file.size.y/2)
+            width = file.size.x
+            height = file.size.y
+        p.image(image=[np.flipud(img)], x=anchor.x, y=anchor.y, 
+                dw=width, dh=height, palette="Greys256")
+
+    def file_input_callback(attr, old, new):
+        """
+        Callback to upload file
         """
         for value, filename in zip(file_input.value, file_input.filename):
             file = DAT_IMG(file_binary=base64.b64decode(value), file_name=filename)
-            if file.nom_size.x > SIZE_THRESHOLD or file.nom_size.y > SIZE_THRESHOLD:
-                img = file.imgs[IMAGE_CHANNEL1]               
-            else:
-                img = file.imgs[IMAGE_CHANNEL0]
-                # img = level_correction(file.imgs[IMAGE_CHANNEL0])
-            # remove any outlier
-            threshold = np.mean(img)+NUM_SIGMA*np.std(img)
-            img[img>threshold] = threshold
-            threshold = np.mean(img)-NUM_SIGMA*np.std(img)
-            img[img<threshold] = threshold
+            nonlocal file_holder
+            file_holder = FILE_TUPLE(file=file, filename=filename)
+            plot_img()
 
-            temp = file.nom_size.y-file.size.y if file.scan_ymode == 2 else 0
-            anchor = XY2D(x=file.offset.x, 
-                          y=(file.offset.y+temp+file.size.y/2))
+    def channel_selection_callback(attr, old, new):
+        """
+        Callback to change channel of image to show
+        """
+        plot_img()
 
-            anchor = point_rot2D_y_inv(anchor, XY2D(x=file.offset.x, y=file.offset.y), 
-                                 np.deg2rad(file.rotation))
-            # print('offset:', file.offset)
-            # print('angle:', file.rotation)
-            if int(file.rotation*100) not in [0, 9000, -9000, 18000, -18000]:
-                temp_file_name = 'image' + filename + '.png'
-                path = os.path.join(os.path.dirname(__file__), 'temp', temp_file_name)
-                
-                plt.imsave(path, img, cmap='gray')
-                p.image_url([temp_file_name], x=anchor.x, y=anchor.y, anchor='center',
-                                         w=file.size.x, h=file.size.y, 
-                                         angle=file.rotation, 
-                                         angle_units='deg',
-                                         name = filename)
-                path_que.append(path)
-                continue                
-            elif int(file.rotation*100) == 0:
-                anchor = XY2D(x=anchor.x-file.size.x/2, y=anchor.y+file.size.y/2)
-                width = file.size.x
-                height = file.size.y
-            elif int(file.rotation*100) == 9000:
-                img = img.swapaxes(-2,-1)[...,::-1,:]
-                anchor = XY2D(x=anchor.x-file.size.y/2, y=anchor.y+file.size.x/2)
-                width = file.size.y
-                height = file.size.x
-            elif int(file.rotation*100) == -9000:
-                img = img.swapaxes(-2,-1)[...,::-1]
-                anchor = XY2D(x=anchor.x-file.size.y/2, y=anchor.y+file.size.x/2)
-                width = file.size.y
-                height = file.size.x
-            else:
-                img = img[...,::-1,::-1]
-                anchor = XY2D(x=anchor.x-file.size.x/2, y=anchor.y+file.size.y/2)
-                width = file.size.x
-                height = file.size.y
-            p.image(image=[np.flipud(img)], x=anchor.x, y=anchor.y, 
-                    dw=width, dh=height, palette="Greys256")
-
+    def connnect_stm_callback(event):
+        """
+        Callback to connect to the STM software
+        """
+        nonlocal stm
+        stm = CreatecWin32()
+        send_xy_bn.disabled=False
+        show_stm_area_bn.disabled=False
 
     rect_que = deque()
+    file_holder = None
+    stm = None
 
     # setup a map with y-axis inverted, and a virtual boundary of the scanner range
     p = figure(match_aspect=True, tools=[PanTool(), UndoTool(), RedoTool(), ResetTool(), SaveTool()])
@@ -159,14 +197,14 @@ def make_document(doc):
     
     # for upload file
     file_input = FileInput(accept=".dat", multiple=True)
-    file_input.on_change('value', upload_data)
+    file_input.on_change('value', file_input_callback)
 
     # buttons for clear marks and send xy coordinates
     clear_marks_bn = Button(label="Clear Marks", button_type="success")
     clear_marks_bn.on_click(clear_callback)
-    send_xy_bn = Button(label="Send XY to STM", button_type="success")
+    send_xy_bn = Button(label="Send XY to STM", button_type="success", disabled=True)
     send_xy_bn.on_click(send_xy_callback)
-    show_stm_area_bn = Button(label="Show STM Location", button_type="success")
+    show_stm_area_bn = Button(label="Show STM Location", button_type="success", disabled=True)
     show_stm_area_bn.on_click(show_area_callback)
 
 
@@ -190,13 +228,20 @@ def make_document(doc):
                               """)
     p.add_tools(HoverTool(callback=hover_coord_cb, tooltips=None))
     p.toolbar_location = None
+
+    ch_select = Select(title="", value="ch0", options=[f'ch{number}' for number in range(MAX_CH)])
+    ch_select.on_change('value', channel_selection_callback)
+
+    connect_stm_bn = Button(label="Connect STM", button_type="success")
+    connect_stm_bn.on_click(connnect_stm_callback)
+
+
     # layout includes the map and the controls below
-    controls_1 = row([file_input, show_stm_area_bn, textxy_show], sizing_mode='stretch_width')
-    controls_2 = row([textxy_hover, clear_marks_bn, send_xy_bn], sizing_mode='stretch_width')
+    controls_1 = row([textxy_hover, file_input, ch_select, textxy_show], sizing_mode='stretch_width')
+    controls_2 = row([clear_marks_bn, connect_stm_bn, show_stm_area_bn, send_xy_bn], sizing_mode='stretch_width')
     doc.add_root(column([p, controls_1, controls_2], sizing_mode='stretch_both'))
 
 
-stm = CreatecWin32()
 path_que = deque()
 apps = {'/': make_document}
 extra_patterns = [(r"/(image(.*))", tornado.web.StaticFileHandler, 
