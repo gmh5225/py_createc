bdf2fb684bf441088eab392165a51d286990383a
Chen <chen.xu@aalto.fi>
2020-09-15 22:37:29 +0300

    Rewind the plot_img() function to that as in 5d3334 with 2 benefits:
    - Use image() method to plot non-tilted image using 2D np.array,
      tilted image is still plotted by saving it as a .png file first.
    
    - New image will always be the topmost image. The method with
      ColumnDataSource so far can not do this.
      Therefore all code related to ColumnDataSource is also removed
      now.

--- scripts/map/map.py
+++ scripts/map/map.py
@@ -107,54 +107,6 @@ def make_document(doc):
         stm.client.setparam('RotCMode', 0)
         status_text.value = 'XY coordinate sent'
 
-    def plot_image_url():
-        """
-        The function to plot image onto the frame using image_url
-        """
-        if file_holder is None:
-            return
-        file = file_holder.file
-        filename = file_holder.filename
-
-        channel = int(ch_select.value[-1])
-        if channel >= file.channels:
-            channel = file.channels-1
-            ch_select.value = f'{ch_select.value[:-1]}{channel}'
-
-        img = file.imgs[channel]
-
-        # remove any outlier
-        threshold = np.mean(img)+NUM_SIGMA*np.std(img)
-        img[img>threshold] = threshold
-        threshold = np.mean(img)-NUM_SIGMA*np.std(img)
-        img[img<threshold] = threshold
-
-        temp = file.nom_size.y-file.size.y if file.scan_ymode == 2 else 0
-        anchor = XY2D(x=file.offset.x, 
-                      y=(file.offset.y+temp+file.size.y/2))
-
-        anchor = point_rot2D_y_inv(anchor, XY2D(x=file.offset.x, y=file.offset.y), 
-                             np.deg2rad(file.rotation))
-        # print('offset:', file.offset)
-        # print('angle:', file.rotation)
-        temp_file_name = f'image{filename}_{channel}.png'
-        path = os.path.join(os.path.dirname(__file__), 'temp', temp_file_name)
-        
-        plt.imsave(path, img, cmap='gray')
-        # p.image_url([temp_file_name], x=anchor.x, y=anchor.y, anchor='center',
-        #                          w=file.size.x, h=file.size.y, 
-        #                          angle=file.rotation, 
-        #                          angle_units='deg',
-        #                          name = filename)
-        path_que.append(path)
-        source.stream(dict(url=[temp_file_name],
-                           name=[filename],
-                           x=[anchor.x],
-                           y=[anchor.y],
-                           w=[file.size.x],
-                           h=[file.size.y],
-                           angle=[file.rotation]))
-
     def plot_img():
         """
         The main function to plot image onto the frame
@@ -219,12 +171,8 @@ def make_document(doc):
             anchor = XY2D(x=anchor.x-file.size.x/2, y=anchor.y+file.size.y/2)
             width = file.size.x
             height = file.size.y
-        source.stream(dict(image=[np.flipud(img)],
-                           name=[filename],
-                           x=[anchor.x],
-                           y=[anchor.y],
-                           dw=[width],
-                           dh=[height]))
+        p.image(image=[np.flipud(img)], x=anchor.x, y=anchor.y, 
+                dw=width, dh=height, palette="Greys256")
 
     def file_input_callback(attr, old, new):
         """
@@ -234,14 +182,14 @@ def make_document(doc):
             file = DAT_IMG(file_binary=base64.b64decode(value), file_name=filename)
             nonlocal file_holder
             file_holder = FILE_TUPLE(file=file, filename=filename)
-            plot_image_url()
+            plot_img()
         status_text.value = 'File uploaded'
 
     def channel_selection_callback(attr, old, new):
         """
         Callback to change channel of image to show
         """
-        plot_image_url()
+        plot_img()
         status_text.value = 'Channel changed'
 
     def connnect_stm_callback(event):
@@ -258,25 +206,10 @@ def make_document(doc):
     """
     Main body below
     """
-    
     rect_que = deque()
     file_holder = None
     stm = None
     
-    dummy_img = np.random.rand(10, 10)
-    # source = ColumnDataSource(dict(image=[dummy_img],
-    #                                name=[''],
-    #                                x=[0],
-    #                                y=[0],
-    #                                dw=[0],
-    #                                dh=[0]))
-    source = ColumnDataSource(dict(url=['image_dummy.png'],
-                                   name=[''],
-                                   x=[0],
-                                   y=[0],
-                                   w=[0],
-                                   h=[0],
-                                   angle=[0]))
     # setup a map with y-axis inverted, and a virtual boundary of the scanner range
     p = figure(match_aspect=True, tools=[PanTool(), UndoTool(), RedoTool(), ResetTool(), SaveTool()])
     p.y_range.flipped = True
@@ -284,18 +217,6 @@ def make_document(doc):
     #               fill_alpha=0, line_color='gray', name='none')
     p.line([-SCAN_BOUNDARY_X, -SCAN_BOUNDARY_X, SCAN_BOUNDARY_X, SCAN_BOUNDARY_X, -SCAN_BOUNDARY_X], 
            [SCAN_BOUNDARY_Y, -SCAN_BOUNDARY_Y, -SCAN_BOUNDARY_Y, SCAN_BOUNDARY_Y, SCAN_BOUNDARY_Y])
-
-    # main_renderer=p.image(source=source, image='image', 
-    #                 x='x', y='y', dw='dw', dh='dh', 
-    #                 name='name', palette="Greys256")
-
-    main_renderer=p.image_url(source=source, url='url', 
-                              x='x', y='y', 
-                              w='w', h='h', 
-                              name='name',
-                              angle='angle',
-                              anchor='center',
-                              angle_units='deg')
 
     # Add the wheel zoom tool
     wheel_zoom_tool = WheelZoomTool(zoom_on_axis=False)
@@ -345,34 +266,6 @@ def make_document(doc):
     # A button to (re)connect to the STM software
     connect_stm_bn = Button(label="(Re)Connect to STM", button_type="success")
     connect_stm_bn.on_click(connnect_stm_callback)
-
-    """
-    # show the filename of the topmost image, but it doesn't work for rotated image.
-    # something needs to be done on the bokeh package to resolve the issue.
-    # so this code below is not in use.
-    """
-    
-    # hover_fiexed_position_cb = CustomJS(code="""
-    #                                     var tooltips = document.getElementsByClassName("bk-tooltip");
-    #                                     for (var i = 0, len = tooltips.length; i < len; i ++) {
-    #                                     tooltips[i].style.top = ""; // unset what bokeh.js sets
-    #                                     tooltips[i].style.left = "";
-    #                                     tooltips[i].style.top = "0vh";
-    #                                     tooltips[i].style.left = "4vh";
-    #                                     }
-    #                                     """)
-    # filename_hover = HoverTool(renderers=[main_renderer], 
-    #                            callback=hover_fiexed_position_cb,
-    #                            point_policy='snap_to_data')
-    # filename_hover.tooltips = """
-    #     <style>
-    #         .bk-tooltip>div:not(:last-child) {display:none;}
-    #     </style>
-    #     @name
-    # """
-    # 
-    # p.add_tools(filename_hover)
-
 
     # show the status of the interface
     status_text = TextInput(title='', value='Ready', disabled=True)
