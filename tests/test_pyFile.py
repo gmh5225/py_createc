3be1f3bcb0ed925c3349deaa6a2aa62b7217b16b
Chen <chen.xu@aalto.fi>
2021-01-10 14:05:26 +0200

    Reformat test file
    Former-commit-id: 09a9a448b07f6e5bd382ebf4f2aa193f864d4ec1

--- tests/test_pyFile.py
+++ tests/test_pyFile.py
@@ -1,19 +1,21 @@
 import numpy as np
+import os
 
 
 def test_DAT_IMG():
-
     """
     To test the class DAT_IMG
     """
     from createc.Createc_pyFile import DAT_IMG
-    
-    file = DAT_IMG('./A200622.081914.dat')
-    with open('A200622.081914.npy', 'rb') as f:
+    this_dir = os.path.dirname(__file__)
+    file = DAT_IMG(os.path.join(this_dir, 'A200622.081914.dat'))
+    with open(os.path.join(this_dir, 'A200622.081914.npy'), 'rb') as f:
         for img in file.imgs:
             npy_img = np.load(f)
             assert img.shape == npy_img.shape
             np.testing.assert_allclose(img, npy_img)
+
+
 """
     with open('A200622.081914.npy', 'wb') as f:
         for img in file.imgs:
