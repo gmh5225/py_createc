50a0f6fece4bb2e03773033d93fd5b0ef5e47784
xuc1 <chen.xu@aalto.fi>
2021-01-11 15:48:14 +0200

    Add VERT_SPEC test
    Former-commit-id: 06c6c1e3dce3540bc5bd8b104feece37979b0f3b

--- tests/test_pyFile.py
+++ tests/test_pyFile.py
@@ -1,13 +1,14 @@
 import numpy as np
 import os
 
+this_dir = os.path.dirname(__file__)
+
 
 def test_DAT_IMG():
     """
     To test the class DAT_IMG
     """
     from createc.Createc_pyFile import DAT_IMG
-    this_dir = os.path.dirname(__file__)
     file = DAT_IMG(os.path.join(this_dir, 'A200622.081914.dat'))
     with open(os.path.join(this_dir, 'A200622.081914.npy'), 'rb') as f:
         for img in file.imgs:
@@ -16,10 +17,23 @@ def test_DAT_IMG():
             np.testing.assert_allclose(img, npy_img)
 
 
+def test_VERT_SPEC():
+    """
+    To test the class VERT_SPEC
+    """
+    from createc.Createc_pyFile import VERT_SPEC
+    import pandas as pd
+    from pandas._testing import assert_frame_equal
+
+    file = VERT_SPEC(os.path.join(this_dir, 'A190824.135614.vert'))
+    readin = pd.read_csv(os.path.join(this_dir, 'A190824.135614.csv'), index_col='idx')
+    assert_frame_equal(readin, file.spec)
+
+
 """
     with open('A200622.081914.npy', 'wb') as f:
         for img in file.imgs:
             np.save(f, img)
 """
 
-test_DAT_IMG()
+# test_DAT_IMG()
