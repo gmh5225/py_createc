b7a2db06284373a4a884017c08fd6e5cbb23f443
xuc1 <chen.xu@aalto.fi>
2021-01-12 14:45:30 +0200

    Some reformat
    Former-commit-id: 70a22db9abe728d70f7f71f329a581f4c541f87c

--- examples/tracking/scan_with_tracking.py
+++ examples/tracking/scan_with_tracking.py
@@ -9,7 +9,7 @@ Autofilesave should be OFF
 
 """
 from createc.Createc_pyFile import DAT_IMG
-#from skimage.feature import register_translation as rt
+# from skimage.feature import register_translation as rt
 from skimage.registration import phase_cross_correlation as pcc
 from skimage.exposure import rescale_intensity as ri
 from skimage.filters import gaussian
@@ -21,21 +21,37 @@ import logging.config
 import yaml
 import sys
 
+
 def level_correction(Y):
     m, n = Y.shape
-    assert m >=2 and n >= 2
+    assert m >= 2 and n >= 2
     X1, X2 = np.mgrid[:m, :n]
-    X = np.hstack((np.reshape(X1, (m*n, 1)), np.reshape(X2, (m*n, 1))))
-    X = np.hstack((np.ones((m*n, 1)), X))
-    YY = np.reshape(Y, (m*n, 1))
+    X = np.hstack((np.reshape(X1, (m * n, 1)), np.reshape(X2, (m * n, 1))))
+    X = np.hstack((np.ones((m * n, 1)), X))
+    YY = np.reshape(Y, (m * n, 1))
     theta = np.dot(np.dot(np.linalg.pinv(np.dot(X.transpose(), X)), X.transpose()), YY)
     plane = np.reshape(np.dot(X, theta), (m, n))
-    return Y-plane
+    return Y - plane
+
 
 def find_shift(img_src, img_des, img_previous, extra_sec, continuous_drift=True):
-    shift = [pcc(level_correction(gaussian(ri(src))), level_correction(gaussian(ri(des))))[0] 
-              for src, des in zip([img_src.img_array_list[i] for i in params['shift_reg_channel']], 
-                                  [img_des.img_array_list[i] for i in params['shift_reg_channel']])]
+    """
+
+    Parameters
+    ----------
+    img_src
+    img_des
+    img_previous
+    extra_sec
+    continuous_drift
+
+    Returns
+    -------
+
+    """
+    shift = [pcc(level_correction(gaussian(ri(src))), level_correction(gaussian(ri(des))))[0]
+             for src, des in zip([img_src.img_array_list[i] for i in params['shift_reg_channel']],
+                                 [img_des.img_array_list[i] for i in params['shift_reg_channel']])]
     shift = np.mean(shift, axis=0)
     dt1 = img_src.timestamp - img_previous.timestamp
     dt2 = time.time() + extra_sec - img_previous.timestamp
@@ -45,43 +61,43 @@ def find_shift(img_src, img_des, img_previous, extra_sec, continuous_drift=True)
 
 with open('./logging_tracking.yaml', 'rt') as f:
     config = yaml.safe_load(f.read())
-logging.config.dictConfig(config)    
+logging.config.dictConfig(config)
 logger = logging.getLogger('main')
- 
+
 with open('./parameters.yaml', 'rt') as f:
     params = yaml.safe_load(f.read())
-    
-   
+
 stm = CreatecWin32()
-template = stm.savedatfilename if params['use_last_as_template'] else params['template_folder']+params['template_file']
+template = stm.savedatfilename if params['use_last_as_template'] else params['template_folder'] + params[
+    'template_file']
 if template == '':
     print('There is no most recent image to be used as template')
     sys.exit()
-    
+
 try:
     img_des = DAT_IMG(template)
 except FileNotFoundError:
     print('Template file cannot be opened.')
     sys.exit()
-    
+
 img_previous = img_des
-logger.info('Start.'+'*'*30) 
-logger.info('template: '+ template[-params['g_filename_len']:])
+logger.info('Start.' + '*' * 30)
+logger.info('template: ' + template[-params['g_filename_len']:])
 
 idx = 0
 Height_Range_Angstrom = np.linspace(params['StartHeight'], params['EndHeight'], params['Total_steps'])
 for ch_zoff in Height_Range_Angstrom:
-    logger.info('-' *10)
-    logger.info('ch_zoff %.2f' % round(ch_zoff,2))
+    logger.info('-' * 10)
+    logger.info('ch_zoff %.2f' % round(ch_zoff, 2))
 
     for ch_bias in params['Bias_Range_mV']:
         idx += 1
-        logger.info('ch_bias %.2f' % round(ch_bias,2))
+        logger.info('ch_bias %.2f' % round(ch_bias, 2))
         logger.info('scan for alignment to template')
-        stm.pre_scan_01(chmode=img_des.chmode, 
-		                    ddeltaX=img_des.ddeltaX,
-                            deltaX_dac=img_des.deltaX_dac, 
-							channels_code=img_des.channels_code)
+        stm.pre_scan_01(chmode=img_des.chmode,
+                        ddeltaX=img_des.ddeltaX,
+                        deltaX_dac=img_des.deltaX_dac,
+                        channels_code=img_des.channels_code)
         time_to_wait = float(stm.client.getparam('Sec/Image:'))
         stm.client.scanstart()
         time.sleep(time_to_wait)
@@ -90,7 +106,7 @@ for ch_zoff in Height_Range_Angstrom:
         stm.client.quicksave()
         cc_file_4align = stm.client.savedatfilename
         logger.info('cc_file_4align: ' + cc_file_4align[-params['g_filename_len']:])
-        
+
         logger.info('Align to template')
         img_src = DAT_IMG(cc_file_4align)
 
@@ -100,7 +116,6 @@ for ch_zoff in Height_Range_Angstrom:
         logger.info('[dy, dx] = {}'.format(shift))
         stm.setxyoffpixel(dx=shift[1], dy=shift[0])
         time.sleep(params['g_reposition_delay'])
-            
 
         # for testing shift registration
         """
@@ -121,11 +136,10 @@ for ch_zoff in Height_Range_Angstrom:
         time.sleep(params['g_reposition_delay'])        
         """
 
-
         logger.info('const current mode scan')
         stm.pre_scan_01(chmode=params['Ccmode']['mode'],
-                            deltaX_dac=params['deltaX_dac'],
-                            channels_code=params['Ccmode']['channels_code'])
+                        deltaX_dac=params['deltaX_dac'],
+                        channels_code=params['Ccmode']['channels_code'])
         time_to_wait = float(stm.client.getparam('Sec/Image:'))
         stm.client.scanstart()
         time.sleep(time_to_wait)
@@ -137,10 +151,10 @@ for ch_zoff in Height_Range_Angstrom:
 
         logger.info('const height mode scan')
         stm.pre_scan_01(chmode=params['Chmode']['mode'],
-                            ddeltaX=params['Chmode']['ddeltaX'],
-                            channels_code=params['Chmode']['channels_code'],
-                            ch_zoff=ch_zoff, 
-                            ch_bias=ch_bias)
+                        ddeltaX=params['Chmode']['ddeltaX'],
+                        channels_code=params['Chmode']['channels_code'],
+                        ch_zoff=ch_zoff,
+                        ch_bias=ch_bias)
         time_to_wait = float(stm.client.getparam('Sec/Image:'))
         stm.client.scanstart()
         time.sleep(time_to_wait)
@@ -149,16 +163,16 @@ for ch_zoff in Height_Range_Angstrom:
         stm.client.quicksave()
         logger.info('ch: ' + stm.client.savedatfilename[-params['g_filename_len']:])
 
-logger.info('Final template scan')        
+logger.info('Final template scan')
 stm.pre_scan_01(chmode=img_des.chmode,
-                    ddeltaX=img_des.ddeltaX,
-                    deltaX_dac=img_des.deltaX_dac,
-                    channels_code=img_des.channels_code)
+                ddeltaX=img_des.ddeltaX,
+                deltaX_dac=img_des.deltaX_dac,
+                channels_code=img_des.channels_code)
 time_to_wait = float(stm.client.getparam('Sec/Image:'))
 stm.client.scanstart()
 time.sleep(time_to_wait)
 while stm.client.scanstatus:
-	time.sleep(5)
+    time.sleep(5)
 stm.client.quicksave()
-logger.info(stm.client.savedatfilename[-params['g_filename_len']:])                      
+logger.info(stm.client.savedatfilename[-params['g_filename_len']:])
 logger.info('Done.')
