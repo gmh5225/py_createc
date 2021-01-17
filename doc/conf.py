6a6a11fbd96e67677deef3b2c5f074ac78f68d1d
Chen <chen.xu@aalto.fi>
2021-01-18 00:49:27 +0200

    Clean the sphinx documentation
    Useful files are
    conf.py
    index.rst
    
    
    Former-commit-id: e1adee576ccfd18da271e184cb54ebd3ce47aecf

--- doc/conf.py
+++ doc/conf.py
@@ -10,21 +10,21 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath(r"C:\\Users\\xuc1\\OneDrive - Aalto University\\Program\\codes\\py_createc\\createc"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Py-Createc'
+project = 'py_createc'
 copyright = '2021, Chen Xu'
 author = 'Chen Xu'
 
 # The full version, including alpha/beta/rc tags
 import createc
-
 release = createc.__version__
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -34,9 +34,9 @@ release = createc.__version__
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'numpydoc'
-]
+    'sphinx.ext.napoleon'
 
+]
 # Generate automatic API documentation
 autosummary_generate = True
 autosummary_imported_members = True
@@ -50,15 +50,26 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+import sphinx_pdj_theme
+html_theme = 'sphinx_pdj_theme'
+htm_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+
+# sets the darker appearence
+html_theme_options = {
+    'style': 'darker'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-html_static_path = []
+html_static_path = ['_static']
+
+
+# -- Extension configuration -------------------------------------------------
--- doc/index.rst
+++ doc/index.rst
@@ -1,13 +1,21 @@
-.. Py-Createc documentation master file, created by
-   sphinx-quickstart on Fri Jan  8 13:58:13 2021.
+.. py_createc documentation master file, created by
+   sphinx-quickstart on Mon Jan 18 00:12:43 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Py-Createc's documentation!
+Welcome to py_createc's documentation!
 ======================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-   api
+   modules
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
