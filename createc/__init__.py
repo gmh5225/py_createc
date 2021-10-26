4137d480ab9fb066534a8a399da6f9f8e5e5c30c
xuc1 <chen.xu@aalto.fi>
2021-10-26 16:13:53 +0300

    Update version number
    Former-commit-id: 4fb425b0bcb6ec8737c540033a6eb640cdef4e98

--- createc/__init__.py
+++ createc/__init__.py
@@ -4,7 +4,7 @@ Created on Wed May 20 22:24:50 2020
 
 @author: xuc1
 """
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 from .Createc_pyCOM import CreatecWin32
 from .Createc_pyFile import DAT_IMG
